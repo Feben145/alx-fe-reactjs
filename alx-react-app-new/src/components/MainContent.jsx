6b56900c44b4ff6cdcb713e1dda3c7a2f9dd1a3a
febenTaye <feben.arsenal@gmail.com>
2025-02-19 21:55:45 +0300

    alx-react-app-new style updated

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -1,7 +1,7 @@
 function MainContent() {
     return (
     <main>
-      <p>I love to visit New York, Paris, and Tokyo.</p>
+      <p style={{ fontSize: '16px', color: 'white', textAlign: 'left' }}>I love to visit New York, Paris, and Tokyo.</p>
     </main>
     );
 }
