7dd5a34ef0fdc8406117983f656216c2bdddef4e
febenTaye <feben.arsenal@gmail.com>
2025-02-22 17:40:28 +0300

    counter added

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,31 +1,13 @@
 import React from 'react';
-import UserProfile from './components/UserProfile'; // Import the UserProfile component
-import Header from  './components/Header';
-import MainContent from  './components/MainContent';
-import Footer from  './components/Footer';
+import Counter from './components/Counter';  // Import Counter component
 
-
-const App = () => {
+function App() {
   return (
-    <div className="App">
-      <h1>User Profiles</h1>
-      <UserProfile
-        name="Alice"
-        age="25"
-        bio="Loves hiking and photography"
-      />
-      <UserProfile
-        name="Bob"
-        age="30"
-        bio="Avid traveler and foodie"
-      />
-      <Header />
-      <MainContent />
-      <Footer />
-           
+    <div>
+      <h1>Counter Application</h1>
+      <Counter /> 
     </div>
-
-  );
-};
+  ); 
+}
 
 export default App;
