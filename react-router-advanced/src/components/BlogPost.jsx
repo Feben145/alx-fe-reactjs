3fad456b7988f49de9732afb783e6ab081639819
febenTaye <feben.arsenal@gmail.com>
2025-03-09 11:35:05 +0300

    react query fetch add router dom6

--- react-router-advanced/src/components/BlogPost.jsx
+++ react-router-advanced/src/components/BlogPost.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
-import { useParams } from 'react-router-dom'; // useParams will fetch the dynamic id
+import { useParams } from 'react-router-dom';
 
 const BlogPost = () => {
-  const { id } = useParams(); // Get the dynamic id parameter from the URL
-
+  const { id } = useParams();  // Get the dynamic 'id' from the URL
+  
   return (
     <div>
       <h2>Blog Post {id}</h2>
-      {/* Simulating a blog post for demonstration */}
-      <p>This is the content of blog post with ID: {id}</p>
+      <p>This is the content for blog post with ID: {id}</p>
     </div>
   );
 };
