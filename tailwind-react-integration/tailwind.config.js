869a3dc5f3ee195deb008755a908e35b6400c717
febenTaye <feben.arsenal@gmail.com>
2025-03-16 23:01:13 +0300

    tailwind integrated2

--- tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-    "./public/index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",  // Ensure this covers all files where Tailwind classes might be used
+    "./public/index.html",         // Include the HTML file
   ],
-  darkMode: 'media', // or 'class' if you want to toggle dark mode via a class
+  darkMode: 'media',  // Or 'class' if you want to toggle dark mode manually
   theme: {
     extend: {},
   },
