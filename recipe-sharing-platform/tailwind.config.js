dd1071eec047d7deb64c38737f7ad4a39d4c4d74
febenTaye <feben.arsenal@gmail.com>
2025-03-21 17:55:58 +0300

    index.html

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -1,8 +1,7 @@
-// tailwind.config.js
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
+    "./public/index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
