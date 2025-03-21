ca068411a0592993a5532cbd085f6f9241f989a2
febenTaye <feben.arsenal@gmail.com>
2025-03-21 19:36:56 +0300

    data.json deduplicated

--- recipe-sharing-platform/postcss.config.js
+++ recipe-sharing-platform/postcss.config.js
@@ -1,12 +1,10 @@
 // postcss.config.js
 import tailwindcss from 'tailwindcss';
 import autoprefixer from 'autoprefixer';
-import tailwindcssPostCSS from '@tailwindcss/postcss';
 
 export default {
   plugins: [
     tailwindcss,
-    autoprefixer,
-    tailwindcssPostCSS
+    autoprefixer
   ]
 };
