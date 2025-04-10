0cd99d3be57d03080678fdcfb357fdd7880db957
febenTaye <feben.arsenal@gmail.com>
2025-04-10 18:31:03 +0300

    style updated

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -3,7 +3,8 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <nav style={{backgroundColor:'#2563EB',  padding: '10px 0', textAlign:'center', width: '100%', position:'fixed', left:'0', top:'0'}}>
+    <nav style={{ display: 'flex',
+        justifyContent: 'center', backgroundColor:'black',  padding: '10px 0', textAlign:'center', width: '100%', position:'fixed', left:'0', top:'0'}}>
       <Link to="/" style={{color:'white', margin:'0 15px', fontSize:'16px'}}>Home</Link>
       <Link to="/services" style={{color:'white', margin:'0 15px', fontSize:'16p'}}>Services</Link>
       <Link to="/contact" style={{color:'white', margin:'0 15px', fontSize:'16p'}}>Contact</Link>
