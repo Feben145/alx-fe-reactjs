2d28b4c81fbe34a6a6c1ab7bc7beea5e5ef30302
febenTaye <feben.arsenal@gmail.com>
2025-04-14 10:42:09 +0300

    set recipe added in recipe platform

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -2,10 +2,14 @@ import { create } from "zustand";
 
 const useRecipeStore = create((set) => ({
   recipes: [],
+
   addRecipe: (recipe) =>
     set((state) => ({
       recipes: [...state.recipes, recipe],
     })),
+
+  // adding setRecipes
+  setRecipes: (newRecipes) => set({ recipes: newRecipes }),
 }));
 
 export default useRecipeStore;
