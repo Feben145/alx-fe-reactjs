e4e0a9ad250ea075c7871b76ebe40ffd53a67f00
febenTaye <feben.arsenal@gmail.com>
2025-04-14 11:06:41 +0300

    update feature added to recipe app

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -3,13 +3,24 @@ import { create } from "zustand";
 const useRecipeStore = create((set) => ({
   recipes: [],
 
+  setRecipes: (newRecipes) => set({ recipes: newRecipes }),
+
   addRecipe: (recipe) =>
     set((state) => ({
       recipes: [...state.recipes, recipe],
     })),
 
-  // adding setRecipes
-  setRecipes: (newRecipes) => set({ recipes: newRecipes }),
+  updateRecipe: (updatedRecipe) =>
+    set((state) => ({
+      recipes: state.recipes.map((recipe) =>
+        recipe.id === updatedRecipe.id ? updatedRecipe : recipe
+      ),
+    })),
+
+  deleteRecipe: (id) =>
+    set((state) => ({
+      recipes: state.recipes.filter((recipe) => recipe.id !== id),
+    })),
 }));
 
 export default useRecipeStore;
